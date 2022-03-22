0851fa9f1fed1a442d4c49cd36bc9613dcabc5a1
lohgrar <99026288+lohgrar@users.noreply.github.com>
2022-03-22 11:44:03 +0200

    atm-api: Add helmet and cors as middleware in app.js

--- atm-api/app.js
+++ atm-api/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 // const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
@@ -9,6 +11,8 @@ const usersRouter = require("./routes/users");
 const app = express();
 
 app.use(logger("dev"));
+app.use(helmet());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser()); (Not used)
