adae9daee749f530c9d6c1c0bfcc21f26860f97a
t1lear00 <t1lear00@students.oamk.fi>
2022-03-29 14:05:08 +0300

    atm-api: lisätty transaction router/model

--- atm-api/app.js
+++ atm-api/app.js
@@ -1,24 +1,22 @@
-const express = require("express");
-const path = require("path");
-// const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const helmet = require("helmet");
-const cors = require("cors");
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
 
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+var transactionRouter = require('./routes/transaction');
 
-const app = express();
+var app = express();
 
-app.use(logger("dev"));
 app.use(helmet());
 app.use(cors());
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser()); (Not used)
-// app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use('/transaction', transactionRouter);
 
 module.exports = app;
