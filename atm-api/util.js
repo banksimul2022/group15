3115100039bac653fe6b88ef740bb58b479939de
lohgrar <99026288+lohgrar@users.noreply.github.com>
2022-03-30 13:18:11 +0300

    atm-api: Move nshcl common utility function in to util.js

--- atm-api/util.js
+++ atm-api/util.js
@@ -1,4 +1,9 @@
 module.exports = {
+    // Emulates the ?? (Nullish coalescing) operator (The operator is only available in node v14 and up)
+    nshcl: (value, default_value) => {
+        return value === null || value === undefined ? default_value : value;
+    },
+
     handleQueryResult: (res, error, result) => {
         if(error) {
             res.json(error);
