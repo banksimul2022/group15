fa45d28e0c84e461155e6699cd769b2e62605db6
hrar <99026288+lohgrar@users.noreply.github.com>
2022-03-31 12:17:49 +0300

    rest: Add Qt library files

---
+++ atm-dll/rest/rest.cpp
@@ -0,0 +1,4 @@
+#include "rest.h"
+
+REST::REST() {
+}
---
+++ atm-dll/rest/rest.h
@@ -0,0 +1,11 @@
+#ifndef REST_H
+#define REST_H
+
+#include "REST_global.h"
+
+class REST_EXPORT REST {
+    public:
+        REST();
+};
+
+#endif // REST_H
