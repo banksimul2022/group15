8c1802cd3aa2975d529d9d1abd4e7894f5e9eec6
lohgrar <99026288+lohgrar@users.noreply.github.com>
2022-04-07 17:54:01 +0300

    atm-ui: Add timer/logout signal to UserStatusBarWidget

--- atm-ui/userstatusbarwidget.h
+++ atm-ui/userstatusbarwidget.h
@@ -14,8 +14,23 @@ class UserStatusBarWidget : public QWidget {
         explicit UserStatusBarWidget(QWidget *parent = nullptr);
         ~UserStatusBarWidget();
 
+        void resetTimeout();
+
+    signals:
+        void logout();
+
+    private slots:
+        void logoutTimerTick();
+
     private:
+        void updateTimeoutLabel();
+
+        static const uint defaultLogoutTimeout = 30;
+
         Ui::UserStatusBarWidget *ui;
+        QMetaObject::Connection btnConnection;
+        QTimer *logoutTimer;
+        uint logoutTimout;
 };
 
 #endif // USERSTATUSBARWIDGET_H
