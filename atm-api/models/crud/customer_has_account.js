9e138f78b9f741036ace80c12379af64c5aaa71e
lohgrar <99026288+lohgrar@users.noreply.github.com>
2022-04-12 21:05:24 +0300

    atm-api: Allow specifying reference key in customer_has_account CRUD operations

--- atm-api/models/crud/customer_has_account.js
+++ atm-api/models/crud/customer_has_account.js
@@ -1,20 +1,58 @@
 const db = require("../../database");
 
 module.exports = {
-    getById: (id) => {
-        return db.query("SELECT * FROM customer_has_account WHERE customerId=?", [ id ]);
+    getById: (key, id) => {
+        if(typeof(key) === "number") {
+            return db.query(
+                "SELECT * FROM customer_has_account WHERE customerId=? AND accountId=?",
+                [ key, id ]
+            );
+        }
+
+        return db.query(
+            key === "customer" ?
+                "SELECT * FROM customer_has_account WHERE customerId=?" :
+                "SELECT * FROM customer_has_account WHERE accountId=?",
+            [ id ]
+        );
     },
     getAll: () => {
         return db.query("SELECT * FROM customer_has_account");
     },
     add: (customer_has_account) => {
-        
-        return db.query("INSERT INTO customer_has_account (customerId, accountId) values(?, ?)", [ customer_has_account.customerId, customer_has_account.accountId]);
+        return db.query(
+            "INSERT INTO customer_has_account (customerId, accountId) values(?, ?)",
+            [ customer_has_account.customerId, customer_has_account.accountId ]
+        );
     },
-    delete: (id) => {
-        return db.query("DELETE FROM customer_has_account WHERE customerId=?", [ id ]);
+    delete: (key, id) => {
+        if(typeof(key) === "number") {
+            return db.query(
+                "DELETE FROM customer_has_account WHERE customerId=? AND accountId=?",
+                [ key, id ]
+            );
+        }
+
+        return db.query(
+            key === "customer" ?
+                "DELETE FROM customer_has_account WHERE customerId=?" :
+                "DELETE FROM customer_has_account WHERE accountId=?",
+            [ id ]
+        );
     },
-    update: ( id, customer_has_account) => {
-        return db.query("UPDATE customer_has_account SET customerId=?, accountId=? WHERE customerId=?", [ customer_has_account.customerId, customer_has_account.accountId, id]);
+    update: (key, id, customer_has_account) => {
+        if(typeof(key) === "number") {
+            return db.query(
+                "UPDATE customer_has_account SET customerId=?, accountId=? WHERE customerId=? AND accountId=?",
+                [ customer_has_account.customerId, customer_has_account.accountId, key, id ]
+            );
+        }
+
+        return db.query(
+            key === "customer" ?
+                "UPDATE customer_has_account SET customerId=?, accountId=? WHERE customerId=?" :
+                "UPDATE customer_has_account SET customerId=?, accountId=? WHERE accountId=?",
+            [ customer_has_account.customerId, customer_has_account.accountId, id ]
+        );
     }
 };
--- atm-api/routes/crud/customer_has_account.js
+++ atm-api/routes/crud/customer_has_account.js
@@ -3,24 +3,43 @@ const router = express.Router();
 const customer_has_account = require("../../models/crud/customer_has_account");
 const butil = require("../../util");
 
-router.get("/:id?", (req, res) => {
+router.get("/:key?/:id?", (req, res) => {
     if(req.params.id) {
-        customer_has_account.getById(req.params.id).then(result => res.json(result)).catch(error => butil.handleQueryError(error, res));
+        if(req.params.key !== "account" && req.params.key !== "customer")
+            throw new butil.PromiseFail("Invalid key type of " + req.params.key, 400);
+
+        customer_has_account.getById(req.params.key, req.params.id)
+            .then(result => res.json(result))
+            .catch(error => butil.handleQueryError(error, res));
     } else {
-        customer_has_account.getAll().then(result => res.json(result)).catch(error => butil.handleQueryError(error, res));
+        customer_has_account.getAll()
+            .then(result => res.json(result))
+            .catch(error => butil.handleQueryError(error, res));
     }
 });
 
 router.post("/", (req, res) => {
-    customer_has_account.add(req.body).then(result => res.json(result)).catch(error => butil.handleQueryError(error, res));
+    customer_has_account.add(req.body)
+        .then(result => res.json(result))
+        .catch(error => butil.handleQueryError(error, res));
 });
 
-router.delete("/:id", (req, res) => {
-    customer_has_account.delete(req.params.id).then(result => res.json(result)).catch(error => butil.handleQueryError(error, res));
+router.delete("/:key/:id", (req, res) => {
+    if(req.params.key !== "account" && req.params.key !== "customer")
+            throw new butil.PromiseFail("Invalid key type of " + req.params.key, 400);
+
+    customer_has_account.delete(req.params.key, req.params.id)
+        .then(result => res.json(result))
+        .catch(error => butil.handleQueryError(error, res));
 });
 
-router.put("/:id", (req, res) => {
-    customer_has_account.update(req.params.id, req.body).then(result => res.json(result)).catch(error => butil.handleQueryError(error, res));
+router.put("/:key/:id", (req, res) => {
+    if(req.params.key !== "account" && req.params.key !== "customer")
+            throw new butil.PromiseFail("Invalid key type of " + req.params.key, 400);
+
+    customer_has_account.update(req.params.key, req.params.id, req.body)
+        .then(result => res.json(result))
+        .catch(error => butil.handleQueryError(error, res));
 });
 
 module.exports = router;
