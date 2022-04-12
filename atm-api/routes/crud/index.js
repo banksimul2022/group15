bdb4b91cceaf6db03cbb49fe7220fc877a087604
lohgrar <99026288+lohgrar@users.noreply.github.com>
2022-04-12 14:23:59 +0300

    atm-api: Add permission checks to crud operations

--- atm-api/routes/crud/index.js
+++ atm-api/routes/crud/index.js
@@ -1,3 +1,5 @@
+const permissions = require("../../permissions");
+const butil = require("../../util");
 const router = require("express").Router();
 
 const customerRouter = require("./customer");
@@ -5,6 +7,12 @@ const accountRouter = require("./account");
 const cardRouter = require("./card");
 const transactionRouter = require("./transaction");
 const customer_has_accountRouter = require("./customer_has_account");
+
+router.use(
+    butil.permissionChecker((req) => {
+        return permissions["crud"][req.method === "GET" ? "read" : "write"][req.path.split("/")[1]];
+    })
+);
 
 router.use("/customer", customerRouter);
 router.use("/account", accountRouter);
