b2f82e25a8fc05702cf951a2ebaf2e333bc65b9f
lohgrar <99026288+lohgrar@users.noreply.github.com>
2022-04-12 19:38:33 +0300

    atm-api: Add PromiseFail error and promisify jwt.verify

--- atm-api/util.js
+++ atm-api/util.js
@@ -1,12 +1,21 @@
 const jwt = require("jsonwebtoken");
 const util = require("util");
 
-class SilentPromiseFail extends Error {
-    constructor(message) {
-        super(message)
-        this.name = 'SILENT_PROMISE_FAIL';
+class PromiseFail extends Error {
+    constructor(message, status) {
+        super(message);
+        this.status = status;
     }
 }
+
+class SilentPromiseFail extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "SILENT_PROMISE_FAIL";
+    }
+}
+
+const jwtVeifyPromise = util.promisify(jwt.verify);
 
 const checkPermissions = (permFlagGetter, req, res, next) => {
     const authHeader = req.headers["authorization"];
@@ -17,35 +26,43 @@ const checkPermissions = (permFlagGetter, req, res, next) => {
         return;
     }
 
-    jwt.verify(authHeader.split(" ")[1], process.env.JWT_SECRET, (err, token) => {
-        if(err) {
+    jwtVeifyPromise(authHeader.split(" ")[1], process.env.JWT_SECRET)
+        .then(token => {
+            let perm = null;
+
+            try {
+                perm = permFlagGetter(req);
+            } catch(e) {
+                throw new SilentPromiseFail(util.format("Failed to run permFlagGetter for '%s'! Details below.\n\n%s", req.path, e));
+            }
+
+            if(perm === undefined || perm === null) {
+                throw new PromiseFail("Failed to determine permission flag for this action", 500);
+            }
+
+            if((token.permissions & perm) <= 0) {
+                throw new PromiseFail("You don't have permission to peform this action on the specified resource", 403);
+            }
+
+            req.token = token;
+            next();
+        })
+        .catch(err => {
+            if(err instanceof PromiseFail) {
+                res.status(err.status);
+                res.json({ error: err.message });
+                return;
+            }
+
             res.status(403);
-            res.json({ error: "Failed to validate the provided token", detail: err });
-            return;
-        }
 
-        let perm = null;
-        try {
-            perm = permFlagGetter(req);
-        } catch(e) {
-            console.error(util.format("Failed to run permFlagGetter for '%s'! Details below.\n\n%s", req.path, e));
-        }
-
-        if(perm === undefined || perm === null) {
-            res.status(500);
-            res.json({ error: "Failed to determine permission flag for this action" });
-            return;
-        }
-        
-        if((token.permissions & perm) <= 0) {
-            res.status(403);
-            res.json({ error: "You don't have permission to peform this action on the specified resource" });
-            return;
-        }
-
-        req.token = token;
-        next();
-    });
+            if(err instanceof SilentPromiseFail) {
+                console.error(err.message);
+                res.json({ error: "Failed to validate the provided token" });
+            } else {
+                res.json({ error: "Failed to validate the provided token", detail: err });
+            }
+        });
 };
 
 module.exports = {
@@ -63,5 +80,6 @@ module.exports = {
         return (req, res, next) => checkPermissions(permFlagGetter, req, res, next);
     },
 
+    PromiseFail,
     SilentPromiseFail
 };
