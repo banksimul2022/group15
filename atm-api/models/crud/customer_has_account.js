d62f7ecd8a043d28ea34ee799657161360b75823
lohgrar <99026288+lohgrar@users.noreply.github.com>
2022-04-13 21:45:17 +0300

    atm-api: Require both ids for delete/update in customer_has_account

--- atm-api/models/crud/customer_has_account.js
+++ atm-api/models/crud/customer_has_account.js
@@ -25,34 +25,13 @@ module.exports = {
             [ customer_has_account.customerId, customer_has_account.accountId ]
         );
     },
-    delete: (key, id) => {
-        if(typeof(key) === "number") {
-            return db.query(
-                "DELETE FROM customer_has_account WHERE customerId=? AND accountId=?",
-                [ key, id ]
-            );
-        }
-
-        return db.query(
-            key === "customer" ?
-                "DELETE FROM customer_has_account WHERE customerId=?" :
-                "DELETE FROM customer_has_account WHERE accountId=?",
-            [ id ]
-        );
+    delete: (customer, account) => {
+        return db.query("DELETE FROM customer_has_account WHERE customerId=? AND accountId=?", [ customer, account ]);
     },
-    update: (key, id, customer_has_account) => {
-        if(typeof(key) === "number") {
-            return db.query(
-                "UPDATE customer_has_account SET customerId=?, accountId=? WHERE customerId=? AND accountId=?",
-                [ customer_has_account.customerId, customer_has_account.accountId, key, id ]
-            );
-        }
-
+    update: (customer, account, customer_has_account) => {
         return db.query(
-            key === "customer" ?
-                "UPDATE customer_has_account SET customerId=?, accountId=? WHERE customerId=?" :
-                "UPDATE customer_has_account SET customerId=?, accountId=? WHERE accountId=?",
-            [ customer_has_account.customerId, customer_has_account.accountId, id ]
+            "UPDATE customer_has_account SET customerId=?, accountId=? WHERE customerId=? AND accountId=?",
+            [ customer_has_account.customerId, customer_has_account.accountId, customer, account ]
         );
     }
 };
--- atm-api/routes/crud/customer_has_account.js
+++ atm-api/routes/crud/customer_has_account.js
@@ -4,22 +4,6 @@ const permissions = require("../../permissions");
 const customer_has_account = require("../../models/crud/customer_has_account");
 const butil = require("../../util");
 
-const permCheck = async (req) => {
-    const results = await customer_has_account.getById(req.params.key, req.params.id);
-
-    if(req.params.key === "account") {
-        if(!results.length || results.length < 1) {
-            butil.throwIfDenied(permissions.crud.all.customer_has_account, req.token);
-        } else {
-            for(let result of results) {
-                butil.checkPermOnMismatch(req, result.customerId, permissions.crud.all.customer_has_account);
-            }
-        }
-    }
-
-    return results;
-};
-
 router.get("/:key?/:id?", (req, res) => {
     if(req.params.id) {
         if(req.params.key !== "account" && req.params.key !== "customer")
@@ -28,8 +12,20 @@ router.get("/:key?/:id?", (req, res) => {
         if(req.params.key === "customer")
             butil.checkPermOnMismatch(req, permissions.crud.all.customer_has_account);
 
-        permCheck(req)
-            .then(results => res.json(results))
+        customer_has_account.getById(req.params.key, req.params.id)
+            .then(results => {
+                if(req.params.key === "account") {
+                    if(!results.length || results.length < 1) {
+                        butil.throwIfDenied(permissions.crud.all.customer_has_account, req.token);
+                    } else {
+                        for(let result of results) {
+                            butil.checkPermOnMismatch(req, result.customerId, permissions.crud.all.customer_has_account);
+                        }
+                    }
+                }
+
+                res.json(results);
+            })
             .catch(error => butil.handleQueryError(error, res));
     } else {
         customer_has_account.getAll()
@@ -44,27 +40,21 @@ router.post("/", (req, res) => {
         .catch(error => butil.handleQueryError(error, res));
 });
 
-router.delete("/:key/:id", (req, res) => {
-    if(req.params.key !== "account" && req.params.key !== "customer")
-            throw new butil.PromiseFail("Invalid key type of " + req.params.key, 400);
+router.delete("/:customer/:account", (req, res) => {
+    if(req.params.customer !== req.token.customerId)
+        butil.throwIfDenied(permissions.crud.all.customer_has_account, req.token);
 
-    if(req.params.key === "customer")
-        butil.checkPermOnMismatch(req, permissions.crud.all.customer_has_account);
-
-    permCheck(req)
-        .then(async _ => res.json(await customer_has_account.delete(req.params.key, req.params.id)))
+    customer_has_account.delete(req.params.customer, req.params.account)
+        .then(result => res.json(result))
         .catch(error => butil.handleQueryError(error, res));
 });
 
-router.put("/:key/:id", (req, res) => {
-    if(req.params.key !== "account" && req.params.key !== "customer")
-            throw new butil.PromiseFail("Invalid key type of " + req.params.key, 400);
+router.put("/:customer/:account", (req, res) => {
+    if(req.params.customer !== req.token.customerId)
+        butil.throwIfDenied(permissions.crud.all.customer_has_account, req.token);
 
-    if(req.params.key === "customer")
-        butil.checkPermOnMismatch(req, permissions.crud.all.customer_has_account);
-
-    permCheck(req)
-        .then(async _ => res.json(await customer_has_account.update(req.params.key, req.params.id, req.body)))
+    customer_has_account.update(req.params.customer, req.params.account, req.body)
+        .then(result => res.json(result))
         .catch(error => butil.handleQueryError(error, res));
 });
 
