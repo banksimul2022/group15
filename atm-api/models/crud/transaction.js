27be3172f94f234e733458c7943bf08ca2e1dd08
lohgrar <99026288+lohgrar@users.noreply.github.com>
2022-04-13 21:21:09 +0300

    atm-api: Fix transaction model queries

--- atm-api/models/crud/transaction.js
+++ atm-api/models/crud/transaction.js
@@ -2,23 +2,23 @@ const db = require("../../database");
 
 module.exports = {
     getById: (id) => {
-        return db.query("SELECT * FROM transaction WHERE transactionid=?", [ id ]);
+        return db.query("SELECT * FROM transaction WHERE transactionId=?", [ id ]);
     },
     getAll: () => {
         return db.query("SELECT * FROM transaction");
     },
     add: (transaction) => {
         return db.query(
-            "INSERT INTO transaction (accountId, timestamp, toAccount, type, value, cardNumber) VALUES(?, ?, ?, ?, ?, ?)",
-            [ transaction.accountId, transaction.timestamp, transaction.toAccount, transaction.type, transaction.value, transaction.cardNumber ]
+            "INSERT INTO transaction (accountId, timestamp, toAccount, type, sum, cardNumber) VALUES(?, ?, ?, ?, ?, ?)",
+            [ transaction.accountId, transaction.timestamp, transaction.toAccount, transaction.type, transaction.sum, transaction.cardNumber ]
         );
     },
     delete: (id) => {
-        return db.query("DELETE FROM transaction WHERE transactionid=?", [id]);
+        return db.query("DELETE FROM transaction WHERE transactionId=?", [id]);
     }, 
     update: (id, transaction) => {
         return db.query(
-            "UPDATE transaction SET accountId=?, timestamp=?, toAccount=?, type=?, sum=?, cardNumber=? WHERE transactionid=?",
+            "UPDATE transaction SET accountId=?, timestamp=?, toAccount=?, type=?, sum=?, cardNumber=? WHERE transactionId=?",
             [ transaction.accountId, transaction.timestamp, transaction.toAccount, transaction.type, transaction.sum, transaction.cardNumber, id ]
         ); 
     } 
