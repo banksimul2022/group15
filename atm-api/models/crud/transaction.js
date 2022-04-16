154e98fddf3dae10fe3cc22ba87503e565770055
lohgrar <99026288+lohgrar@users.noreply.github.com>
2022-04-16 23:27:12 +0300

    atm-api: Add transactions endpoint to api

--- atm-api/models/crud/transaction.js
+++ atm-api/models/crud/transaction.js
@@ -21,5 +21,13 @@ module.exports = {
             "UPDATE transaction SET accountId=?, timestamp=?, toAccount=?, type=?, sum=?, cardNumber=? WHERE transactionId=?",
             [ transaction.accountId, transaction.timestamp, transaction.toAccount, transaction.type, transaction.sum, transaction.cardNumber, id ]
         ); 
-    } 
+    },
+    fromOffset: async (accountId, offset, max) => {
+        // Use 'WHERE id > offset LIMIT max' instead of 'LIMIT offset,max' to increase query performance
+        // https://stackoverflow.com/a/4502392
+        return await db.query(
+            "SELECT * FROM transaction WHERE transactionId > ? AND accountId = ? ORDER BY transactionId LIMIT ?",
+            [ offset, accountId, max ]
+        );
+    }
 };
--- atm-api/routes/api/data.js
+++ atm-api/routes/api/data.js
@@ -1,3 +1,4 @@
+const transaction = require("../../models/crud/transaction");
 const customer = require("../../models/crud/customer");
 const account = require("../../models/crud/account");
 const butil = require("../../util");
@@ -16,6 +17,38 @@ router.get("/info", (req, res) => {
             });
         })
         .catch(error => butil.handleQueryError(error, res));
+});
+
+router.get("/transactions", (req, res) => {
+    // Limit offset to 0 or higher (Also substract 1 for prev check)
+    const offset = Math.max(Number(butil.nshcl(req.query.offset, 0)) - 1, 0);
+    // Limit amount to 1-30
+    const itemCount = Math.min(Math.max(Number(butil.nshcl(req.query.count, 10)), 1), 30);
+
+    transaction.fromOffset(
+        req.token.accountId,
+        offset,
+        itemCount + 1 // Add 1 for next check
+    )
+    .then(async results => {
+        if(results.length < 1) {
+            res.json({ transactions: results, count: 0, nextOffset: 0 });
+            return;
+        }
+
+        const hasPrev = results[0].transactionId < offset;
+        const hasNext = results.length - hasPrev > itemCount;
+        const retData = results.slice(hasPrev, results.length - hasNext);
+
+        res.json({
+            transactions: retData,
+            count: retData.length,
+            nextOffset: results[results.length - 1].transactionId + 1,
+            hasPrev,
+            hasNext
+        });
+    })
+    .catch(error => butil.handleQueryError(error, res));
 });
 
 module.exports = router;
